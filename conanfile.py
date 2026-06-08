f104bd06f122aafa261ae3c9b3a2d76857165396
rodyherrera <contact@rodyherrera.com>
2026-06-08 01:10:20 -0400

    fix: update structure-identification and polyhedral-template-matching versions in conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -13,8 +13,8 @@ class GrainSegmentationConan(ConanFile):
         "boost/1.88.0",
         "onetbb/2021.12.0",
         "coretoolkit/1.0.0",
-        "structure-identification/1.0.0",
-        "polyhedral-template-matching/1.0.3",
+        "structure-identification/1.0.1",
+        "polyhedral-template-matching/1.0.4",
         "spdlog/1.14.1",
         "nlohmann_json/3.11.3",
     )
