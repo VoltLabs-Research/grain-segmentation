e2c6ca86f0ba529231ea4d525b3b75475f6e3a73
rodyherrera <contact@rodyherrera.com>
2026-08-16 01:45:48 -0400

    refactor: derive the conan version from vpm.json

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,12 @@
+import json
+import os
+
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 
 
 class GrainSegmentationConan(ConanFile):
     name = "grain-segmentation"
-    version = "2.0.5"
     package_type = "static-library"
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
@@ -18,7 +20,13 @@ class GrainSegmentationConan(ConanFile):
         "spdlog/1.14.1",
         "nlohmann_json/3.11.3",
     )
+    exports = "vpm.json"
     exports_sources = "CMakeLists.txt", "include/*", "src/*"
+
+    def set_version(self):
+        manifest = os.path.join(self.recipe_folder, "vpm.json")
+        with open(manifest, encoding="utf-8") as stream:
+            self.version = json.load(stream)["version"]
 
     def layout(self):
         cmake_layout(self)
